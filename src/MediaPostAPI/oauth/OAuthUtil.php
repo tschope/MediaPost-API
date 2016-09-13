2d64ddbc10945cbe20c413dbb2b97f81eae19412
Rodrigo Tschope <rodrigo.tschope@ag2nurun.com.br>
2016-09-13 15:05:30 -0300

    corrigindo class

--- src/MediaPostAPI/oauth/OAuthUtil.php
+++ src/MediaPostAPI/oauth/OAuthUtil.php
@@ -7,7 +7,7 @@ class OAuthUtil
     public static function urlencode_rfc3986($input)
     {
         if (is_array($input)) {
-            return array_map(array('OAuthUtil', 'urlencode_rfc3986'), $input);
+            return array_map(array(__CLASS__, 'urlencode_rfc3986'), $input);
         } else if (is_scalar($input)) {
             return str_replace(
                 '+',
